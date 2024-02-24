b843e0256a79c7df8cc89382cdd492e20580e8df
xsamueljr <samueljiro.alfa@gmail.com>
2024-02-24 23:37:46 +0100

    fixed ci/cd (i hope)

--- src/strings.rs
+++ src/strings.rs
@@ -26,13 +26,15 @@ pub fn count(text: String, substring: &str) -> usize {
 /// 
 /// let text = String::from("Hello World");
 /// let delimiter = " ";
-/// let words = split(text, delimiter);
+/// let words = split(text, Some(delimiter));
 /// 
 /// let expected = vec![String::from("Hello"), String::from("World")];
 /// assert_eq!(words, expected);
 /// assert_eq!(words.len(), 2);
 /// ```
-pub fn split(text: String, delimiter: &str) -> Vec<String> {
+pub fn split(text: String, delimiter: Option<&str>) -> Vec<String> {
+    let delimiter = delimiter.unwrap_or(" ");
+
     if text.is_empty() || delimiter.is_empty() {
         return vec![];
     }
